33d48b7666fd46918f2010b511793a96383b005b
MVShilova <shilova.mv@bk.ru>
2024-07-18 22:17:21 +0300

    Add files via upload

---
+++ zadacha2.py
@@ -0,0 +1,5 @@
+number_of_homework = 12
+time_required = 1.5
+course_name = 'Python'
+time_for_one_task = (time_required / number_of_homework)
+print ('Курс:',course_name, ', всего задач:',number_of_homework, ", затрачено часов:", time_required, 'среднее время выполнения', time_for_one_task, "часа.")
